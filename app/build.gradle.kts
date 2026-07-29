8f8596649a6a30cc244e8785e385532c0f9a4d2e
devilblade28 <229557618+attaul-fullstack-dev@users.noreply.github.com>
2026-07-29 10:41:09 +0700

    Add SSHJ and Bouncy Castle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("com.hierynomus:sshj:0.38.0")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
 }
